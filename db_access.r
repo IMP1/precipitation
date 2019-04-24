1a491e79a9d56fba380f6753a810aeba49914a00
IMP1 <IMP1@mail.com>
2019-04-24 11:32:38 +0100

    table_name is now a param

--- db_access.r
+++ db_access.r
@@ -1,17 +1,17 @@
-save_to_database <- function(data, database_name) {
+save_to_database <- function(data, database_name, table_name) {
   con <- dbConnect(RSQLite::SQLite(), dbname=database_name)
   
   # SQLite can't handle dates, so convert dates to strings.
   sanitised_data <- data.frame(data)
   sanitised_data$Date = as.character(sanitised_data$Date)
   
-  dbWriteTable(con, TABLE_NAME, sanitised_data, overwrite=TRUE)
+  dbWriteTable(con, table_name, sanitised_data, overwrite=TRUE)
   dbDisconnect(con)
 }
 
-load_from_database <- function(database_name) {
+load_from_database <- function(database_name, table_name) {
   con <- dbConnect(RSQLite::SQLite(), dbname=database_name)
-  data <- dbReadTable(con, TABLE_NAME)
+  data <- dbReadTable(con, table_name)
   dbDisconnect(con)
   # SQLite can't handle dates, so convert to dates from strings.
   data$Date = as.Date(data$Date)
